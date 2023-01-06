293bbe0913740a322eb6004523a42a1ae86e9a11
cptop75 <97861881+cptop75@users.noreply.github.com>
2023-01-06 11:16:21 +0900

    Update streamlit_cv1.py

--- streamlit_cv1.py
+++ streamlit_cv1.py
@@ -23,7 +23,7 @@ st.markdown("#### (목표) Computer Vision 기능 강화 (SAHI 알고리즘)")
 " - 작은 슬라이스로 나누면 분석 이미지의 작은 객체의 픽셀 영역이 상대적으로 확대"
 " - 기존 방식 대비 6.8% ~ 14.5% 효율 증가"
 " - 매개변수 : Slice_size (256, 512), overlap_ratio (0.00 ~ 1.00), etc"
-image = Image.open('image/ex1.png')
+image = Image.open('image/ex1.PNG')
 st.image(image, caption='이론')
 
 st.write("")
@@ -42,7 +42,7 @@ st.markdown(
 
 st.write("")
 st.markdown("### 낙동강 홍수통제소 CCTV : 대구시(강창교)")
-image1 = Image.open('image/local.png')
+image1 = Image.open('image/local.PNG')
 st.image(image1, caption='위치')
 
 st.write("")
